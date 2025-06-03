9676e8233d0406f3a11e77c30fbb3f521f65dca4
skybloc4 <117281120+skybloc4@users.noreply.github.com>
2025-06-03 11:40:14 +0200

    score

---
+++ Droper-Prototype/Assets/Script/Score.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    int score;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
